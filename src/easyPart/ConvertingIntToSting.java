6d049ee07b9a210dc8b253bc1ac7bf4839f05632
shakir <shakir@mds-mbp-5>
2020-05-13 01:06:45 -0400

    Added code

---
+++ src/easyPart/ConvertingIntToSting.java
@@ -0,0 +1,17 @@
+package easyPart;
+
+public class ConvertingIntToSting {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int a1= 35;
+		System.out.println(a+a1);
+		
+		//after converting
+		String b =String.valueOf(a);
+		System.out.println(b+a1);
+
+	}
+
+}
---
+++ src/easyPart/ConvertingStingToInt.java
@@ -0,0 +1,18 @@
+package easyPart;
+
+public class ConvertingStingToInt {
+
+	public static void main(String[] args) {
+		String str = "10";
+		int num = 25;
+		
+		System.out.println(str+num);
+		
+		//after converting
+		int num2 = Integer.parseInt(str);
+		System.out.println(num+num2);
+		
+
+	}
+
+}
